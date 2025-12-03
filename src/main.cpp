c3dcee1ad12e9e66238547375a092ca5d9d6ed92
Surwiwer <Januszewski.ben@gmail.com>
2025-12-03 16:07:52 +0100

    Erster Prototyp für Player Movement, sowie Prototyp png, Sprite struct und Cooldown Class.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,24 @@
 #include "raylib.h"
 
 #include "config.h"
-
+#include <math.h>
+#include"cooldown.h"
+#include <vector>
+#include"Sprite.h"
 int main() {
+    bool facessouth;
+    bool facesnorth;
+    bool faceswest;
+    bool faceseast=true;
+    bool flip;
+    float rotation;
+    Cooldown attackCD (1.0f);
+    /*
+    GetMonitorWidth(0);
+    GetMonitorHeight(0);
+    SetConfigFlags(FLAG_WINDOW_UNDECORATED);
+    InitWindow(GetScreenWidth(), GetScreenHeight(), "Borderless Fullscreen");
+    */
     // Raylib initialization
     // Project name, screen size, fullscreen mode etc. can be specified in the config.h.in file
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
@@ -15,7 +31,26 @@ int main() {
 #ifdef GAME_START_FULLSCREEN
     ToggleFullscreen();
 #endif
+    std::vector<Sprite> balls;
+    Sprite ball;
+    // Your own initialization code here
+    ball.texture = LoadTexture("assets/graphics/player.png");
+    ball.pos = {10, 100};
+    ball.speed = 7;
+    ball.frames=0;
+    ball.frameCount=0;
+    ball.frameSpeed = 8;
+    ball.size={0.0f,0.0f, (float)ball.texture.width,(float)ball.texture.height};
 
+    Sprite shot;
+    shot.texture = LoadTexture("assets/graphics/shot.png");
+
+
+    Rectangle srcH = {0,0,(float)shot.texture.width,(float)shot.texture.height};
+    Rectangle wallleft = {0,0,(float) 1,(float)GetScreenHeight()};
+    Rectangle wallright = { .x= (float) GetScreenWidth(), .y= 1, .width= (float) 1, (float)GetScreenHeight() };
+    Rectangle wallup = {0,0,(float) GetScreenWidth(),(float) 1};
+    Rectangle walldown = {0,(float) GetScreenHeight(),(float) GetScreenWidth(),(float) 1};
     // Your own initialization code here
     // ...
     // ...
@@ -27,6 +62,103 @@ int main() {
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
+         ball.frameCount++;
+        if (ball.frameCount >=(60/ball.frameSpeed)) {
+            ball.frameCount =0;
+            ball.frames++;
+            if (ball.frames>8) ball.frames=0;
+            ball.size.x = (float) ball.frames*(float)ball.texture.width/8;
+        }
+        /*
+        if (IsKeyDown(KEY_RIGHT)) ball.frameSpeed ++;
+        else if (IsKeyDown(KEY_LEFT)) ball.frameSpeed --;
+        if (ball.frameSpeed>MAX_FRAME_SPEED) ball.frameSpeed = MAX_FRAME_SPEED;
+        else if (ball.frameSpeed<MIN_FRAME_SPEED) ball.frameSpeed = MIN_FRAME_SPEED;
+        */
+
+        Rectangle player = {ball.pos.x, ball.pos.y, (float)ball.texture.width/9, (float)ball.texture.height/2};
+
+
+        bool collision1 = CheckCollisionRecs( player,  wallleft);
+        bool collision2 = CheckCollisionRecs( player,  wallright);
+        bool collision3 = CheckCollisionRecs( player,  wallup);
+        bool collision4 = CheckCollisionRecs( player,  walldown);
+
+        if (IsKeyDown(KEY_W)) {
+            ball.pos.y -= ball.speed;
+            faceswest=false;
+            faceseast=false;
+            facessouth=false;
+            facesnorth=true;
+            rotation=270;
+            if (collision3 == true) {
+                ball.pos.y=ball.pos.y+ball.speed;
+            }
+        }
+        if (IsKeyDown(KEY_S)) {
+            ball.pos.y += ball.speed;
+            faceswest=false;
+            faceseast=false;
+            facesnorth=false;
+            facessouth=true;
+            rotation=90;
+            if (collision4 == true) {
+                ball.pos.y=ball.pos.y-ball.speed;
+            }
+        }
+        if (IsKeyDown(KEY_A)) {
+            ball.pos.x -= ball.speed;
+            faceseast=false;
+            facesnorth=false;
+            facessouth=false;
+            faceswest=true;
+            flip=true;
+            rotation=180;
+            if (collision1 == true) {
+                ball.pos.x=ball.pos.x+ball.speed;
+            }
+
+        }
+        if (IsKeyDown(KEY_D)) {
+            ball.pos.x += ball.speed;
+            faceswest=false;
+            facesnorth=false;
+            facessouth=false;
+            faceseast=true;
+            flip=false;
+            rotation=0;
+            if (collision2 == true) {
+                ball.pos.x=ball.pos.x-ball.speed;
+            }
+        }
+        if (IsKeyPressed(KEY_Q)) {
+            if (faceseast==true)
+                ball.pos.x = ball.pos.x+ball.texture.width/8 + 500;
+            if (ball.pos.x > wallright.x)
+                ball.pos.x=wallright.x - ball.texture.width/8;
+            if (faceswest==true)
+                ball.pos.x = ball.pos.x-ball.texture.width/8 - 500;
+            if (ball.pos.x < wallleft.x)
+                ball.pos.x=wallleft.x;
+            if (facesnorth==true)
+                ball.pos.y =  ball.pos.y - ball.texture.height - 500;
+            if (ball.pos.y < wallup.y)
+                ball.pos.y=wallup.y;
+            if (facessouth==true)
+                ball.pos.y = ball.pos.y + ball.texture.height/2 + 500;
+            if (ball.pos.y > walldown.y)
+                ball.pos.y=walldown.y - ball.texture.height/2;
+        }
+        if (faceseast==true) {shot.pos = {ball.pos.x+ball.texture.width, ball.pos.y};}
+        if (facessouth==true) {shot.pos = {ball.pos.x, ball.pos.y+ball.texture.height};}
+        if (faceswest==true) {shot.pos = {ball.pos.x-ball.texture.width-ball.speed, ball.pos.y};}
+        if (facesnorth==true) {shot.pos = {ball.pos.x, ball.pos.y-ball.texture.height-ball.speed};}
+
+
+        Rectangle dstH = {shot.pos.x+shot.texture.width / 2.0f,shot.pos.y+shot.texture.height/2.0f,(float)shot.texture.width,(float)shot.texture.height};
+
+
+
         if (IsKeyDown(KEY_LEFT_ALT) && IsKeyPressed(KEY_ENTER)) { //Fullscreen logic.
             if (IsWindowFullscreen()) {
                 ToggleFullscreen();
@@ -47,8 +179,25 @@ int main() {
         BeginTextureMode(canvas);
         { //Within this block is where we draw our app to the canvas and YOUR code goes.
             ClearBackground(WHITE);
-            DrawText("Hello, world!", 10, 10, 30, LIGHTGRAY);
-            DrawTexture(myTexture, 10, 100, WHITE);
+             if (flip==true) {
+                 ball.size ={ball.size.x,0.0f, (float)-ball.texture.width/8,(float)ball.texture.height/2};
+             }else ball.size ={ball.size.x,0.0f, (float)ball.texture.width/8,(float)ball.texture.height/2};
+
+             DrawTextureRec(ball.texture, ball.size, ball.pos, WHITE);  // Draw part of the texture
+
+
+
+             if (attackCD.Ready())
+                 DrawText("Ready", 20,20,24,GREEN);
+             else DrawText(TextFormat("Cooldown %.2f",attackCD.Remaining()), 20,20,24,GREEN);
+             //DrawTextureV(ball.texture, ball.pos, WHITE);
+             DrawText("Hello, world!", GetScreenWidth()/2, GetScreenHeight()/2, 30, LIGHTGRAY);
+             if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && attackCD.Ready()) {
+
+                 DrawTexturePro(shot.texture, srcH, dstH ,{(float)shot.texture.width /2,(float)shot.texture.height/2}, rotation, WHITE );
+                 attackCD.Trigger();
+
+             } attackCD.Update(GetFrameTime());
         }
         EndTextureMode();
         //The following lines put the canvas in the middle of the window and have the negative as black

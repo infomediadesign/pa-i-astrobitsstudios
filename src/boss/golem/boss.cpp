aa52ffdd16408e378ba047b20287f8ef9136bff2
Surwiwer <Januszewski.ben@gmail.com>
2026-01-13 19:52:58 +0100

    try to fix stuff nothing new

---
+++ src/boss/golem/boss.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Ben on 04.12.2025.
+//
+
+#include "boss.h"
+
+
+#include "raylib.h"
+
+#include "config.h"
+
+
+
+
+
+    void Enemy::Init()
+    {
+        texture  = LoadTexture("assets/graphics/testimage.png");
+        pos = {Game::ScreenWidth/2, Game::ScreenHeight/2};
+    }
+
+    void Enemy::Update(float dt)
+    {
+
+    }
+    void Enemy::Draw() {
+        DrawTextureV(texture,pos,WHITE);
+    }
+    void Enemy::Unload() {
+        UnloadTexture(texture);
+    }

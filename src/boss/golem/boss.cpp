a5b05fb6f7ebcfd58079067a761d49cc35050c26
BenColyn <bencolyn4@gmail.com>
2026-01-16 10:04:50 +0100

    Schaden vom Boss ist im code, sollte mit Angriff funktionieren

--- src/boss/golem/boss.cpp
+++ src/boss/golem/boss.cpp
@@ -13,19 +13,57 @@
 
 
 
+
     void Enemy::Init()
     {
         texture  = LoadTexture("assets/graphics/testimage.png");
         pos = {Game::ScreenWidth/2, Game::ScreenHeight/2};
+
+health = 100.0f;
+maxHealth = 100.0f;
+hitTimer = 0.0f;
+active = true;
     }
 
-    void Enemy::Update(float dt)
-    {
+    void Enemy::Update(float dt) {
+        if (!active) return;
+
+        //Wenn der Boss getroffen wurde, zählt der Timer runter
+        if (hitTimer >0) {
+            hitTimer -= dt;
+        }
 
     }
     void Enemy::Draw() {
-        DrawTextureV(texture,pos,WHITE);
+        if (!active) return;
+
+        // DrawTextureV(texture,pos,WHITE);
+
+
+        // EFFEKT: Wenn hitTimer > 0, färbe die Textur Rot ein
+        Color tint = (hitTimer > 0) ? RED : WHITE;
+        DrawTextureV(texture, pos, tint);
+
+        // OPTIONAL: Lebensbalken zeichnen
+        float barWidth = 60.0f;
+        DrawRectangle(pos.x + (texture.width/2) - (barWidth/2), pos.y - 15, barWidth, 6, DARKGRAY);
+        DrawRectangle(pos.x + (texture.width/2) - (barWidth/2), pos.y - 15, barWidth * (health/maxHealth), 6, GREEN);
     }
+
+void Enemy::TakeDamage(float amount) {
+        health -= amount;
+        hitTimer = 0.1f; // Kurz aufblitzen lassen
+
+        if (health <= 0) {
+            active = false; // Boss "stirbt"
+        }
+    }
+
+
     void Enemy::Unload() {
         UnloadTexture(texture);
+    }
+
+Rectangle Enemy::GetRect() {
+        return { pos.x, pos.y, (float)texture.width, (float)texture.height };
     }
--- src/boss/golem/boss.h
+++ src/boss/golem/boss.h
@@ -10,11 +10,24 @@ struct Enemy {
         Texture2D texture;
         Vector2 pos;
 
+
+        // Neue Variablen für Schaden und Lebensbalken
+        float health;
+        float maxHealth;
+        float hitTimer;      // Steuert das rote Aufblinken
+        bool active;         // Ist der Boss noch am Leben?
+
         void Init();
         void Update(float dt);
         void Draw();
         void Unload();
-    };
 
+
+// Funktion, um Schaden zu verursachen
+void TakeDamage(float amount);
+
+// Hilfsfunktion für die Hitbox (für Kollisionen mit Schüssen)
+Rectangle GetRect();
+};
 
 

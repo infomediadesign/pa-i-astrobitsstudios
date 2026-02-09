80298114edb468802d078e78e9d9ee85099251e6
BenColyn <bencolyn4@gmail.com>
2026-02-09 11:40:36 +0100

    header kreirt und background eingefügt noch nicht in den code

---
+++ src/enviroment/background.cpp
@@ -0,0 +1,6 @@
+//
+// Created by benco on 09.02.2026.
+//
+
+#include "background.h"
+
---
+++ src/enviroment/background.h
@@ -0,0 +1,14 @@
+//
+// Created by benco on 09.02.2026.
+//
+
+#ifndef RAYLIBSTARTER_BACKGROUND_H
+#define RAYLIBSTARTER_BACKGROUND_H
+
+
+class background {
+    void draw() override;
+};
+
+
+#endif //RAYLIBSTARTER_BACKGROUND_H

4d9122e41e24aeac22af38baaaa9d2d512140587
BenColyn <bencolyn4@gmail.com>
2026-03-04 19:49:40 +0100

    hp vom spieler richtig

--- src/difficulty/difficulty.cpp
+++ src/difficulty/difficulty.cpp
@@ -7,18 +7,21 @@
 
 void difficulty::setDifficultyEasy(Player &player, Boss &boss) {
     player.maxHp = 150;
+    player.hp = player.maxHp;
     boss.setMaxHealth(150);
     player.setInvincibleDuration(1.0f);
 }
 
 void difficulty::setDifficultyNormal(Player &player, Boss &boss) {
     player.maxHp = 100;
+    player.hp = player.maxHp;
     boss.setMaxHealth(200);
     player.setInvincibleDuration(0.8f);
 }
 
 void difficulty::setDifficultyHard(Player &player, Boss &boss) {
     player.maxHp = 70;
+    player.hp = player.maxHp;
     boss.setMaxHealth(250);
     player.setInvincibleDuration(0.5f);
 }
